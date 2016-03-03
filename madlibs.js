12e4c97c919b1faa6d445b0c702c24f94ff2eed0
mominaf <mominaf8259@tywls.org>
2016-03-03 21:50:08 +0000

    Decluterred code and made buttons function

--- madlibs.js
+++ madlibs.js
@@ -1,14 +1,23 @@
 function generateMadlib() {
   
-  // This is the starting story
-  var story = "Once upon a time in a land far, far away there were a bunch of PLURAL_NOUN.It was birthday  VERB  ";
+  
+  var story = "Once upon a time in a land far, far away there were a bunch of PLURAL_NOUN.It was NAME 's birthday party. Everyone was VERB. Suddenly, a screeching TYPE_OF_NOISE broke the party up and everyone looked at NOUN.   ";
   
 
-  var inputValue = document.getElementById("plural-noun-1").value;
+  var inputValue = document.getElementById("plural-noun-1").value; 
   story = story.replace("PLURAL_NOUN", inputValue);
   
-  inputValue = document.getElementById("verb").value;
-  story = story.replace("VERB", inputValue);
+  inputValue = document.getElementById("name").value;
+  story = story.replace("NAME", inputValue);
+  
+    var inputValue = document.getElementById("noise-1").value; 
+  story = story.replace("TYPE_OF_NOISE", inputValue);
+  
+   var inputValue = document.getElementById("noun-1").value; 
+  story = story.replace("NOUN", inputValue);
+  
+  
+  
   
   // This writes the story when the user clicks the "Generate MadLib" button"
   document.getElementById("result").innerHTML = story;
